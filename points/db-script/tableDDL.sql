cf7b582f46c0c603a74fd729a4624768954498d0
kunyiduan <kunyiduan@163.com>
2020-07-13 15:39:37 +0800

    建表语句修改

--- points/db-script/tableDDL.sql
+++ points/db-script/tableDDL.sql
@@ -4,4 +4,4 @@ CREATE TABLE `points` (
     `points` INT UNSIGNED NOT NULL COMMENT '积分',
     `create_time` DATETIME NOT NULL COMMENT '创建时间',
     `update_time` DATETIME NOT NULL COMMENT '更新时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='积分信息表';
