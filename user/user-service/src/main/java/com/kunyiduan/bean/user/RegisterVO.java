9858d8bcc015c4bde9e86adff750e017f44dbd00
kunyiduan <kunyiduan@163.com>
2020-07-20 22:50:36 +0800

    优化swagger中birthday显示

--- user/user-service/src/main/java/com/kunyiduan/bean/user/RegisterVO.java
+++ user/user-service/src/main/java/com/kunyiduan/bean/user/RegisterVO.java
@@ -1,5 +1,6 @@
 package com.kunyiduan.bean.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kunyiduan.annotation.phone.Phone;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -22,7 +23,7 @@ public class RegisterVO {
     private String telephone;
 
     @ApiModelProperty(required = true, position = 2, value = "密码")
-    @Length(min = 64, max = 64,message = "密码加密错误")
+    @Length(min = 64, max = 64, message = "密码加密错误")
     private String password;
 
     @ApiModelProperty(required = false, position = 3, value = "邮箱")
@@ -35,7 +36,8 @@ public class RegisterVO {
     @ApiModelProperty(required = false, position = 5, value = "头像")
     private String photo;
 
-    @ApiModelProperty(required = false, position = 6, value = "生日")
+    @ApiModelProperty(required = false, position = 6, value = "生日", dataType = "date",example = "2020-07-20")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date birthday;
 
 }
