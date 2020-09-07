a1b71ab167dc087ea597cfe1eed69a633fe817dd
kunyiduan <kunyiduan@163.com>
2020-09-08 00:22:58 +0800

    to(*):高版本ES在yml中配置过期，在config中使用RestHignLevelClient注解替代

---
+++ product-search/product-search-service/src/main/java/com/kunyiduan/config/ElasticsearchConfig.java
@@ -0,0 +1,33 @@
+package com.kunyiduan.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration configuration = ClientConfiguration.builder()
+                //9200-使用http请求，所以rest方式的client使用这个端口进行访问
+                //9300-使用tcp请求，是系统预留给ES内部组件之间的通信方式
+                .connectedTo("localhost:9200")
+                //.withConnectTimeout(Duration.ofSeconds(5))
+                //.withSocketTimeout(Duration.ofSeconds(3))
+                //.useSsl()
+                //.withDefaultHeaders(defaultHeaders)
+                //.withBasicAuth(username, password)
+                // ... other options
+                .build();
+        RestHighLevelClient client = RestClients.create(configuration).rest();
+        return client;
+    }
+
+}
